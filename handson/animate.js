fe5bcd86e59c6f6eb4456215e440c77675140a08
Nino Zijderveld <Nino-zijderveld@hotmail.com>
2021-07-20 21:47:09 +0200

    animate.js changed

--- handson/animate.js
+++ handson/animate.js
@@ -1,28 +1,9 @@
 var design = anime({
-  targets: "#cycle #firstRoad, #XMLID_34_",
-  translateY: -7,
-  easing: "easeInOutSine",
-  duration: 1000,
-  delay: function(el, i) {
-    return i * 250;
-  },
-  direction: "alternate",
-  loop: true
+  targets: "#cycle #tyre1,#tyre2"
+  // TODO
 });
 
 var design = anime({
-  targets: "#cycle #tyre1,#tyre2",
-  rotate: 360,
-  easing: "linear",
-  loop: true,
-  direction: "reverse"
-});
-
-var design = anime({
-  targets: "#cycle #padel",
-  rotate: 360,
-  easing: "linear",
-  loop: true,
-  duration: 3000,
-  direction: "reverse"
+  targets: "#cycle #padel"
+  // TODO
 });
